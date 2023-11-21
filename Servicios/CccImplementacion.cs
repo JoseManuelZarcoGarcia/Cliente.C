ddeafd6c88f8b1966fa1eec74058250fa9769102
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2023-11-21 13:34:39 +0100

    no message

--- Servicios/CccImplementacion.cs
+++ Servicios/CccImplementacion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cliente.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,45 @@ using System.Threading.Tasks;
 
 namespace Cliente.Servicios
 {
-    internal class CccImplementacion
+    internal class CccImplementacion : CccInterfaz
     {
+
+
+        public void darAltaCcc(List<CccDto> listaAntigua)
+        {
+            // creo un cliente
+            CccDto ccc = crearNuevoCcc();
+            // añado el cliente a listaAntigua
+            listaAntigua.Add(ccc);
+
+
+        }
+
+        private CccDto crearNuevoCcc()
+        {
+
+            CccDto nuevoCliente = new CccDto();
+
+            Console.WriteLine("introduzca una id: ");
+            nuevoCliente.IdCcc = (Convert.ToInt64(Console.ReadLine()));
+
+            Console.WriteLine("introduzca su isban: ");
+            nuevoCliente.IsbanCuentaBancaria = Console.ReadLine();
+
+            Console.WriteLine("introduzca su codigo Switch: ");
+            nuevoCliente.CodigoSwitch = Console.ReadLine();
+
+            Console.WriteLine("introduzca su dni: ");
+            nuevoCliente.DniCliente = Console.ReadLine();
+
+            Console.WriteLine("introduzca la fecha de alta: ");
+            nuevoCliente.FchAltaCcc = Console.ReadLine();
+
+
+            return nuevoCliente;
+
+        }
+
+
     }
 }
